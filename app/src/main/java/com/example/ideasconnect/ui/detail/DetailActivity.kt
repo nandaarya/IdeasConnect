e097abe447dae3d2e868f087a06bf3def3bcc7bb
Nanda Arya Putra <nandaarya404@gmail.com>
2024-01-15 23:21:54 +0700

    Add Detail activity and layout

---
+++ app/src/main/java/com/example/ideasconnect/ui/detail/DetailActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ideasconnect.ui.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ideasconnect.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
