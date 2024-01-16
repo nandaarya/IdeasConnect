2c4f570fd2ca9e22601eaafa94b95e00df98697d
Nanda Arya Putra <nandaarya404@gmail.com>
2024-01-16 20:59:43 +0700

    Add Submit Idea layout

---
+++ app/src/main/java/com/example/ideasconnect/ui/submit/SubmitIdeaActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ideasconnect.ui.submit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ideasconnect.R
+
+class SubmitIdeaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_submit_idea)
+    }
+}
